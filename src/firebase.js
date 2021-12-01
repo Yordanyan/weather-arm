574d8bbf5277434958c89b774298bf25f255324b
unknown <david.yordanyan@mail.ru>
2021-12-01 18:31:46 +0300

    first

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2puO8994vfLelq7Fd4W-J56u_6xJGnWI",
+    authDomain: "weather-arm-cf6fd.firebaseapp.com",
+    projectId: "weather-arm-cf6fd",
+    storageBucket: "weather-arm-cf6fd.appspot.com",
+    messagingSenderId: "981661858786",
+    appId: "1:981661858786:web:2ec33a5d2bc41913cb17bb"
+  };
